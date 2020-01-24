e00ffd839a6cc9d29ec688fcdfbca50806a7ca60
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-24 09:26:39 -0600

    CAP-308 src/cap-angular-schematic-auth-auth0/files/cap-auth.module.ts Add a module to the template so that it works correctly.

--- src/cap-angular-schematic-auth-auth0/files/cap-auth.module.ts
+++ src/cap-angular-schematic-auth-auth0/files/cap-auth.module.ts
@@ -7,13 +7,15 @@ import { LoginComponent } from './login/login.component';
 import { ForgotComponent } from './forgot/forgot.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     AuthRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [
     RegisterComponent,
