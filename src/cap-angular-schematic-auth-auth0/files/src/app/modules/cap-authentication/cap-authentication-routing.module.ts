0fe4786ebf2a93e518955d816312c47a1c21a952
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2021-01-20 16:52:29 -0600

    CAP src/cap-angular-schematic-auth-auth0/files/src/app/modules/cap-authentication/cap-authentication-routing.module.ts Create a new path that will render the ChangePasswordComponent component.

--- src/cap-angular-schematic-auth-auth0/files/src/app/modules/cap-authentication/cap-authentication-routing.module.ts
+++ src/cap-angular-schematic-auth-auth0/files/src/app/modules/cap-authentication/cap-authentication-routing.module.ts
@@ -1,11 +1,12 @@
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 import { RegisterComponent } from './register/register.component';
-import { ForgotComponent } from './forgot/forgot.component';
-import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ForgotComponent } from './forgot/forgot.component';
 import { VerifyComponent } from './verify/verify.component';
+import { LoginComponent } from './login/login.component';
 import { GuardService } from 'cap-authentication';
 
 const routes: Routes = [
@@ -15,6 +16,7 @@ const routes: Routes = [
       { path: 'register', component: RegisterComponent},
       { path: 'login', component: LoginComponent},
       { path: 'forgot-password', component: ForgotComponent},
+      { path: 'change-password', component: ChangePasswordComponent},
       { path: 'profile', component : ProfileComponent, canActivate: [GuardService]},
       { path: 'verify', component: VerifyComponent, canActivate: [GuardService]}
     ]
